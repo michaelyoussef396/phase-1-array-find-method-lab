b3e03f497d4b1a37161eba7d0d104ad862826325
michaelyoussef396 <michaelyoussef396@gmail.com>
2023-11-01 12:47:28 +1100

    finished

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record) {
+    // Find the first object in the record array with a "W" result and return its year
+    const broncosWin = record.find(game => game.result === "W");
+  
+    // If a win is found, return the year; otherwise, return undefined
+    return broncosWin ? broncosWin.year : undefined;
+  }
+  
